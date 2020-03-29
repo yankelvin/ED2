05dff2cce5373f1d57fc696c69b647b3dec6e5dc
YanKelvin <yan.kelvin@hotmail.com>
2020-03-29 17:50:13 -0300

    Questão da lavajato

---
+++ src/LavaJato/LavaJato.cs
@@ -0,0 +1,70 @@
+﻿using GenericTree.Class;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LavaJato
+{
+    public class LavaJato
+    {
+        public Tree<string, List<string>> Suspeitos { get; private set; }
+
+        public LavaJato()
+        {
+            Suspeitos = new Tree<string, List<string>>("Suspeitos", null);
+        }
+
+        public void AdicionarSuspeito(string nomeSuspeito, List<string> crimesCumplice)
+        {
+            Suspeitos.AddNode("Suspeitos", nomeSuspeito, crimesCumplice);
+        }
+
+        public void AdicionarCumplice(string nomeSuspeito, string nomeCumplice, List<string> crimesCumplice)
+        {
+            var crimesSuspeito = Suspeitos.SearchNode(nomeSuspeito).Data;
+
+            foreach (var crime in crimesSuspeito)
+            {
+                if (crimesCumplice.Contains(crime))
+                {
+                    crimesCumplice.Add("Formação de Quadrilha");
+                    crimesSuspeito.Add("Formação de Quadrilha");
+                    break;
+                }
+            }
+
+            Suspeitos.AddNode(nomeSuspeito, nomeCumplice, crimesCumplice);
+        }
+
+        public List<string> ListarCumplices(string nomeSuspeito)
+        {
+            var list = new List<string>();
+            var cumplices = Suspeitos.SearchNode(nomeSuspeito).Childrens;
+
+            foreach (var cump in cumplices)
+                list.Add(cump.Ind);
+
+            list.Sort();
+
+            return list;
+        }
+
+        public List<string> ListarCrimes(string nomeSuspeito)
+        {
+            var crimes = Suspeitos.SearchNode(nomeSuspeito).Data;
+
+            crimes.Sort();
+
+            return crimes;
+        }
+
+        public List<string> ListarSuspeitosComum(string nomeSuspeito1, string nomeSuspeito2)
+        {
+            var cumplices1 = Suspeitos.SearchNode(nomeSuspeito1).Childrens.Select(it => it.Ind);
+            var cumplices2 = Suspeitos.SearchNode(nomeSuspeito2).Childrens.Select(it => it.Ind);
+
+            var comum = cumplices1.Intersect(cumplices2);
+
+            return comum.ToList();
+        }
+    }
+}
---
+++ src/LavaJato/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LavaJato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lj = new LavaJato();
+
+            while (true)
+            {
+                Console.WriteLine("------------ LAVA JATO ------------");
+                Console.WriteLine("1 - Cadastrar Suspeito");
+                Console.WriteLine("2 - Adicionar Cúmplice");
+                Console.WriteLine("3 - Listar cúmplices de um suspeito");
+                Console.WriteLine("4 - Listar crimes de um suspeito");
+                Console.WriteLine("5 - Listar suspeitos em comum entre duas partes");
+                Console.WriteLine("6 - Informar alcance de um suspeito");
+                Console.WriteLine("7 - Sair");
+
+
+                Console.WriteLine("\nInforme a opção desejada: ");
+                int opcao = Convert.ToInt32(Console.ReadLine());
+
+                if (opcao == 7)
+                    break;
+
+                switch (opcao)
+                {
+                    case 1:
+                        Console.WriteLine("Nome do suspeito: ");
+                        var nomeSuspeito = Console.ReadLine();
+
+                        Console.WriteLine("Crimes (Separe por virgulas): ");
+                        var stringao = Console.ReadLine();
+                        var crimes = stringao.Split(",");
+
+                        lj.AdicionarSuspeito(nomeSuspeito, new List<string>(crimes));
+
+                        break;
+                    case 2:
+                        Console.WriteLine("Nome do suspeito: ");
+                        nomeSuspeito = Console.ReadLine();
+
+                        Console.WriteLine("Nome do cúmplice: ");
+                        var nomeCumplice = Console.ReadLine();
+
+                        Console.WriteLine("Crimes (Separe por virgulas): ");
+                        stringao = Console.ReadLine();
+                        crimes = stringao.Split(",");
+
+                        lj.AdicionarCumplice(nomeSuspeito, nomeCumplice, new List<string>(crimes));
+
+                        break;
+                    case 3:
+                        Console.WriteLine("Nome do suspeito: ");
+                        nomeSuspeito = Console.ReadLine();
+
+                        var cumplices = lj.ListarCumplices(nomeSuspeito);
+                        Console.WriteLine("Cúmplices:");
+                        foreach (var item in cumplices)
+                            Console.WriteLine($"- {item}");
+
+                        break;
+                    case 4:
+                        Console.WriteLine("Nome do suspeito: ");
+                        nomeSuspeito = Console.ReadLine();
+
+                        var crimesS = lj.ListarCrimes(nomeSuspeito);
+                        Console.WriteLine("Crimes:");
+                        foreach (var item in crimesS)
+                            Console.WriteLine($"- {item}");
+
+                        break;
+                    case 5:
+                        Console.WriteLine("Nome do suspeito 1: ");
+                        var nomeSuspeito1 = Console.ReadLine();
+
+                        Console.WriteLine("Nome do suspeito 2: ");
+                        var nomeSuspeito2 = Console.ReadLine();
+
+                        var comum = lj.ListarSuspeitosComum(nomeSuspeito1, nomeSuspeito2);
+
+                        Console.WriteLine("Em comum:");
+                        foreach (var item in comum)
+                            Console.WriteLine($"- {item}");
+                        break;
+                    case 6:
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida!");
+                        break;
+                }
+            }
+        }
+    }
+}
