267edf5b2eb3a6808dbeb99f6c961c9c0e86bb9d
YanKelvin <yan.kelvin@hotmail.com>
2020-05-24 14:30:32 -0300

    pa-atv1-padroes-projeto
    Segunda questão da atividade.

---
+++ src/pa-atv1-padroes-projeto/Models/Impressora.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pa_atv1_padroes_projeto.Models
+{
+    public class Impressora
+    {
+        private static Impressora _impressora = null;
+        public static string Marca { get; private set; }
+        public static string IP { get; private set; }
+        public static int Porta { get; private set; }
+
+        private Impressora() { }
+
+        public static Impressora GetImpressora()
+        {
+            if (_impressora == null)
+                _impressora = new Impressora();
+
+            return _impressora;
+        }
+
+        public static void SetMarca(string marca)
+        {
+            if (!string.IsNullOrEmpty(marca))
+                Marca = marca;
+        }
+
+        public static void SetIp(string ip)
+        {
+            if (!string.IsNullOrEmpty(ip))
+                IP = ip;
+        }
+
+        public static void SetPorta(int porta)
+        {
+            if (porta > 0)
+                Porta = porta;
+        }
+
+        public void Print(string matricula)
+        {
+            Console.WriteLine($"Matrícula: {matricula} | Memória: {GetHashCode()} | Marca: {Marca} | Ip: {IP} | Porta: {Porta}");
+        }
+    }
+
+    public class Funcionario
+    {
+        public string Nome { get; private set; }
+        public string Matricula { get; private set; }
+
+        public Funcionario(string nome, string matricula)
+        {
+            Nome = nome;
+            Matricula = matricula;
+        }
+    }
+}
--- src/pa-atv1-padroes-projeto/Program.cs
+++ src/pa-atv1-padroes-projeto/Program.cs
@@ -7,15 +7,40 @@ namespace pa_atv1_padroes_projeto
     {
         static void Main(string[] args)
         {
-            var televisao = new Televisao("Samsung");
-            televisao.AdicionarCanal("TV Sergipe");
+            Console.Write("Informe a questão: ");
+            var opcao = Convert.ToInt32(Console.ReadLine());
 
-            var tel1 = new Telespectador("Nilda", televisao);
-            var tel2 = new Telespectador("Marcio", televisao);
-            var tel3 = new Telespectador("Jasiel", televisao);
+            switch (opcao)
+            {
+                case 1:
+                    var televisao = new Televisao("Samsung");
+                    televisao.AdicionarCanal("TV Sergipe");
 
-            televisao.AdicionarCanal("TV Aperipê");
-            televisao.AdicionarCanal("TV Atalaia");
+                    _ = new Telespectador("Nilda", televisao);
+                    _ = new Telespectador("Marcio", televisao);
+                    _ = new Telespectador("Jasiel", televisao);
+
+                    televisao.AdicionarCanal("TV Aperipê");
+                    televisao.AdicionarCanal("TV Atalaia");
+                    break;
+                case 2:
+                    Impressora.SetMarca("HP Deskjet F300 Series");
+                    Impressora.SetIp("10.211.55.1");
+                    Impressora.SetPorta(9100);
+
+                    var fun1 = new Funcionario("Nilda", "123");
+                    Impressora.GetImpressora().Print(fun1.Matricula);
+
+                    var fun2 = new Funcionario("Marcio", "456");
+                    Impressora.GetImpressora().Print(fun2.Matricula);
+
+                    var fun3 = new Funcionario("Jasiel", "789");
+                    Impressora.GetImpressora().Print(fun3.Matricula);
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida!");
+                    break;
+            }
         }
     }
 }
